4b09078c6f243e903f410e31c6e95c1c120d4ff0
Edouard Leurent <eleurent@gmail.com>
2018-05-18 16:12:26 +0200

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rl-agents',
+    version='1.0.dev0',
+    description='A collection of Reinforcement Learning agents',
+    url='https://github.com/eleurent/rl-agents',
+    author='Edouard Leurent',
+    author_email='eleurent@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Researchers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='reinforcement learning agents',
+    packages=find_packages(exclude=['docs', 'scripts', 'tests*']),
+    install_requires=['gym', 'numpy', 'pygame', 'matplotlib'],
+    extras_require={
+        'dev': ['scipy',],
+    },
+    entry_points={
+        'console_scripts': [],
+    },
+)
+
