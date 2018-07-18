056ae787c2734bf0c71fd4dcf20913309918f040
Edouard Leurent <eleurent@gmail.com>
2018-07-18 14:20:21 +0200

    Display linear model trajectory uncertainty as a colormap wrt time

--- rl_agents/agents/linear/graphics.py
+++ rl_agents/agents/linear/graphics.py
@@ -1,8 +1,4 @@
-import numpy as np
-from matplotlib import pyplot as plt, gridspec as gridspec
-
 import pygame
-import matplotlib as mpl
 import matplotlib.cm as cm
 
 
@@ -10,6 +6,7 @@ class LinearModelGraphics(object):
     """
         Graphical visualization of the LinearModelAgent interval observer.
     """
+    TIME_MAP = cm.hot_r
     RED = (255, 0, 0)
     BLUE = (0, 0, 255)
     MIN_VALUE = -2
@@ -20,7 +17,7 @@ class LinearModelGraphics(object):
         agent.road_observer.compute_trajectories(15, 1/15)
         for observer in agent.road_observer.observers.values():
             model_traj, min_traj, max_traj = observer.get_trajectories()
-            cls.display_bounding_polygons(min_traj, max_traj, surface, cls.RED)
+            cls.display_boxes(min_traj, max_traj, surface, cls.TIME_MAP)
             cls.display_trajectory(model_traj, surface, cls.BLUE)
 
     @classmethod
@@ -32,17 +29,20 @@ class LinearModelGraphics(object):
                              1)
 
     @classmethod
-    def display_boxes(cls, min_traj, max_traj, surface, color):
-        for i in range(len(min_traj)):
-            pygame.draw.rect(surface, color,
-                             (surface.vec2pix(min_traj[i].position),
-                              (surface.pix(max_traj[i].position[0] - min_traj[i].position[0]),
-                              surface.pix(max_traj[i].position[1] - min_traj[i].position[1]))), 0)
+    def display_boxes(cls, min_traj, max_traj, surface, cmap):
+        for i in reversed(range(len(min_traj))):
+            color = cmap(i/len(min_traj), bytes=True)
+            rect = (surface.vec2pix(min_traj[i].position),
+                    (surface.pix(max_traj[i].position[0] - min_traj[i].position[0]),
+                     surface.pix(max_traj[i].position[1] - min_traj[i].position[1])))
+            if rect[1] != (0, 0):
+                pygame.draw.rect(surface, color, rect, 0)
 
     @classmethod
-    def display_bounding_polygons(cls, min_traj, max_traj, surface, color):
+    def display_bounding_polygons(cls, min_traj, max_traj, surface, cmap, boxes=True):
         for (A, B) in [(min_traj, max_traj), (min_traj, min_traj)]:
-            for i in range(len(min_traj)-1):
+            for i in reversed(range(len(min_traj)-1)):
+                color = cmap(i / len(min_traj), bytes=True)
                 input_points = [[(A[i].position[0], min_traj[i].position[1]), (A[i].position[0], max_traj[i].position[1])],
                                 [(B[i].position[0], min_traj[i].position[1]), (A[i].position[0], max_traj[i].position[1])],
                                 [(A[i].position[0], min_traj[i].position[1]), (B[i].position[0], max_traj[i].position[1])]]
@@ -55,3 +55,9 @@ class LinearModelGraphics(object):
                         p.append(p[0])
                         p = list(map(surface.vec2pix, p))
                         pygame.draw.polygon(surface, color, p, 0)
+                if boxes:
+                    rect = (surface.vec2pix(min_traj[i+1].position),
+                            (surface.pix(max_traj[i+1].position[0] - min_traj[i+1].position[0]),
+                             surface.pix(max_traj[i+1].position[1] - min_traj[i+1].position[1])))
+                    if rect[1] != (0, 0):
+                        pygame.draw.rect(surface, color, rect, 0)
