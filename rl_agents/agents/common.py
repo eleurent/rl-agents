9a87001c6093a54552435655e680097d0abed043
Edouard Leurent <eleurent@gmail.com>
2018-08-23 17:36:56 +0200

    Log more information when env cannot be configured

--- rl_agents/agents/common.py
+++ rl_agents/agents/common.py
@@ -65,8 +65,8 @@ def load_environment(env_path):
         env.configure(env_config)
         # Reset the environment to ensure configuration is applied
         env.reset()
-    except AttributeError:
-        pass
+    except AttributeError as e:
+        logger.info("This environment does not support configuration. {}".format(e))
     return env
 
 
