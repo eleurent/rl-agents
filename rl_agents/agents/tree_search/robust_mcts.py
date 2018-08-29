8f6192b00ee60d7e6011d7707f9ddb77343274f2
Edouard Leurent <eleurent@gmail.com>
2018-08-29 15:10:29 +0200

    Move OneStepRobustMCTS at the end
    Not a great agent...

--- rl_agents/agents/tree_search/robust_mcts.py
+++ rl_agents/agents/tree_search/robust_mcts.py
@@ -6,74 +6,6 @@ from rl_agents.agents.common import load_agent, preprocess_env
 from rl_agents.agents.tree_search.mcts import MCTSAgent, MCTS, Node
 
 
-class OneStepRobustMCTS(AbstractAgent):
-    def __init__(self,
-                 env,
-                 config=None):
-        """
-            A new MCTS agent with multiple environment models.
-        :param env: The true environment
-        :param config: The agent configuration.
-                       It should include an "agents" key giving a list of paths to several MCTSAgent configurations.
-                       This class could be modified to directly load configuration dictionaries instead of agents
-                       config files.
-        """
-        super(OneStepRobustMCTS, self).__init__(config)
-        self.agents = [load_agent(agent_config_path, env) for agent_config_path in self.config["agents"]]
-        self.__env = env
-
-    @classmethod
-    def default_config(cls):
-        return dict(agents=[])
-
-    @property
-    def env(self):
-        return self.__env
-
-    @env.setter
-    def env(self, env):
-        self.__env = env
-        for agent in self.agents:
-            agent.env = env
-
-    def plan(self, state):
-        for agent in self.agents:
-            agent.plan(state)
-
-        min_action_values = {k: np.inf for k in range(self.env.action_space.n)}
-        for agent in self.agents:
-            min_action_values = {k: min(v, agent.mcts.root.children[k].value)
-                                 for k, v in min_action_values.items()
-                                 if k in agent.mcts.root.children}
-        action = max(min_action_values.keys(), key=(lambda key: min_action_values[key]))
-        for agent in self.agents:
-            agent.previous_action = action
-
-        return [action]
-
-    def reset(self):
-        for agent in self.agents:
-            agent.reset()
-
-    def seed(self, seed=None):
-        for agent in self.agents:
-            seeds = agent.seed(seed)
-            seed = seeds[0]
-        return seed
-
-    def record(self, state, action, reward, next_state, done):
-        raise NotImplementedError()
-
-    def act(self, state):
-        return self.plan(state)[0]
-
-    def save(self, filename):
-        raise NotImplementedError()
-
-    def load(self, filename):
-        raise NotImplementedError()
-
-
 class DiscreteRobustMCTSAgent(MCTSAgent):
     def __init__(self,
                  env,
@@ -169,3 +101,71 @@ class IntervalRobustMCTS(AbstractAgent):
 
     def record(self, state, action, reward, next_state, done):
         return self.sub_agent.record(state, action, reward, next_state, done)
+
+
+class OneStepRobustMCTS(AbstractAgent):
+    def __init__(self,
+                 env,
+                 config=None):
+        """
+            A new MCTS agent with multiple environment models.
+        :param env: The true environment
+        :param config: The agent configuration.
+                       It should include an "agents" key giving a list of paths to several MCTSAgent configurations.
+                       This class could be modified to directly load configuration dictionaries instead of agents
+                       config files.
+        """
+        super(OneStepRobustMCTS, self).__init__(config)
+        self.agents = [load_agent(agent_config_path, env) for agent_config_path in self.config["agents"]]
+        self.__env = env
+
+    @classmethod
+    def default_config(cls):
+        return dict(agents=[])
+
+    @property
+    def env(self):
+        return self.__env
+
+    @env.setter
+    def env(self, env):
+        self.__env = env
+        for agent in self.agents:
+            agent.env = env
+
+    def plan(self, state):
+        for agent in self.agents:
+            agent.plan(state)
+
+        min_action_values = {k: np.inf for k in range(self.env.action_space.n)}
+        for agent in self.agents:
+            min_action_values = {k: min(v, agent.mcts.root.children[k].value)
+                                 for k, v in min_action_values.items()
+                                 if k in agent.mcts.root.children}
+        action = max(min_action_values.keys(), key=(lambda key: min_action_values[key]))
+        for agent in self.agents:
+            agent.previous_action = action
+
+        return [action]
+
+    def reset(self):
+        for agent in self.agents:
+            agent.reset()
+
+    def seed(self, seed=None):
+        for agent in self.agents:
+            seeds = agent.seed(seed)
+            seed = seeds[0]
+        return seed
+
+    def record(self, state, action, reward, next_state, done):
+        raise NotImplementedError()
+
+    def act(self, state):
+        return self.plan(state)[0]
+
+    def save(self, filename):
+        raise NotImplementedError()
+
+    def load(self, filename):
+        raise NotImplementedError()
