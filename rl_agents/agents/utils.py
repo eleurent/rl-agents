0618275ee0d0b0fe760aaad7a3d43641ccac18ff
Edouard Leurent <eleurent@gmail.com>
2018-10-18 15:57:36 +0200

    Modify OLOP planner so as to use KL-UCB instead of Chernoff-Hoeffding-UCB

--- rl_agents/agents/utils.py
+++ rl_agents/agents/utils.py
@@ -98,4 +98,83 @@ def wrap_to_pi(x):
     return ((x+np.pi) % (2*np.pi)) - np.pi
 
 
+def bernoulli_kullback_leibler(p, q):
+    """
+        Compute the Kullback-Leibler divergence of two Bernoulli distributions.
+
+    :param p: parameter of the first Bernoulli distribution
+    :param q: parameter of the second Bernoulli distribution
+    :return: KL(B(p), B(q))
+    """
+    kl1, kl2 = 0, np.infty
+    if p > 0:
+        if q > 0:
+            kl1 = p*np.log(p/q)
+
+    if q < 1:
+        if p < 1:
+            kl2 = (1 - p) * np.log((1 - p) / (1 - q))
+        else:
+            kl2 = 0
+    return kl1 + kl2
+
+
+def d_bernoulli_kullback_leibler_dq(p, q):
+    """
+        Compute the partial derivative of the Kullback-Leibler divergence of two Bernoulli distributions.
+
+        With respect to the parameter q of the second distribution.
+
+    :param p: parameter of the first Bernoulli distribution
+    :param q: parameter of the second Bernoulli distribution
+    :return: dKL/dq(B(p), B(q))
+    """
+    return (1 - p) / (1 - q) - p/q
+
+
+def hoeffding_upper_bound(_sum, count, time):
+    """
+        Upper Confidence Bound of the empirical mean built on the Chernoff-Hoeffding inequality.
+
+    :param _sum: Sum of sample values
+    :param count: Number of samples
+    :param time: Allows to set the bound confidence level to time^-4
+    """
+    return _sum / count + np.sqrt(2 * np.log(time) / count)
+
+
+def kl_upper_bound(_sum, count, time, eps=1e-2):
+    """
+        Upper Confidence Bound of the empirical mean built on the Kullback-Leibler divergence.
+
+        The computation involves solving a small convex optimization problem using Newton Iteration
+
+    :param _sum: Sum of sample values
+    :param count: Number of samples
+    :param time: Allows to set the bound confidence level
+    :param eps: Absolute accuracy of the Netwon Iteration
+    """
+    mu = _sum/count
+    max_div = np.log(time)/count
+
+    # Solve KL(mu, q) = max_div
+    q = mu
+    next_q = (1 + mu)/2
+    while abs(q - next_q) > eps:
+        q = next_q
+
+        # Newton Iteration
+        klq = bernoulli_kullback_leibler(mu, q) - max_div
+        d_klq = d_bernoulli_kullback_leibler_dq(mu, q)
+        next_q = q - klq / d_klq
+
+        # Out of bounds: move toward the bound
+        weight = 0.9
+        if next_q > 1:
+            next_q = weight*1 + (1 - weight)*q
+        elif next_q < mu:
+            next_q = weight*mu + (1 - weight)*q
+
+    return constrain(q, 0, 1)
+
 
---
+++ tests/agents/test_utils.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pytest
+
+from rl_agents.agents.utils import bernoulli_kullback_leibler, d_bernoulli_kullback_leibler_dq, kl_upper_bound
+
+
+def test_bernoulli_kullback_leibler():
+    assert bernoulli_kullback_leibler(0, 1) == np.infty
+    q = np.random.random()
+    assert bernoulli_kullback_leibler(0, q) > 0
+    assert bernoulli_kullback_leibler(q, q) == 0
+
+    x = np.random.uniform(0, 1, 10)
+    x.sort()
+    for i in range(np.size(x) - 1):
+        assert bernoulli_kullback_leibler(x[0], x[i]) < bernoulli_kullback_leibler(x[0], x[i+1])
+
+
+def test_d_bernoulli_kullback_leibler_dq():
+    x = np.random.uniform(0, 1, 2)
+    p, q = x
+    eps = 1e-6
+    assert d_bernoulli_kullback_leibler_dq(p, q) == \
+        pytest.approx((bernoulli_kullback_leibler(p, q+eps) - bernoulli_kullback_leibler(p, q-eps)) / (2*eps), 1e-3)
+
+
+def test_kl_upper_bound():
+    assert kl_upper_bound(0.5 * 1, 1, 10, eps=1e-3) == pytest.approx(0.997, 1e-3)
+    assert kl_upper_bound(0.5 * 10, 10, 20, eps=1e-3) == pytest.approx(0.835, 1e-3)
+    assert kl_upper_bound(0.5 * 10, 20, 40, eps=1e-3) == pytest.approx(0.549, 1e-3)
+
+    rands = np.random.randint(1, 500, 2)
+    rands.sort()
+    mu, count, time = np.random.random(), rands[0], rands[1]
+    ucb = kl_upper_bound(mu*count, count, time, eps=1e-3)
+    assert not np.isnan(ucb)
+    d_max = np.log(time) / count
+    assert bernoulli_kullback_leibler(mu, ucb) == pytest.approx(d_max, 1e-2)
