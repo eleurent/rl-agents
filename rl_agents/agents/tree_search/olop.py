8386cf576170febccb147f1bb18d6cbf7e4bbfd0
Edouard Leurent <eleurent@gmail.com>
2018-10-28 18:16:22 +0100

    Add OLOP configs to choose upper-bound type and tree construction strategy
    Allows to choose between original OLOP and KL-OLOP with lazy tree expansion

--- rl_agents/agents/tree_search/olop.py
+++ rl_agents/agents/tree_search/olop.py
@@ -23,19 +23,48 @@ class OLOP(AbstractPlanner):
         self.env = env
         super(OLOP, self).__init__(config)
 
+    @classmethod
+    def default_config(cls):
+        cfg = super(OLOP, cls).default_config()
+        cfg.update({"upper_bound": "hoeffding",
+                    "lazy_tree_construction": False})
+        return cfg
+
     def make_root(self):
         root = OLOPNode(parent=None, planner=self)
         self.leaves = [root]
         if "horizon" not in self.config:
             self.allocate_budget()
+
+        if not self.config["lazy_tree_construction"]:
+            self.prebuild_tree(self.env.action_space.n)
+
         return root
+
+    def prebuild_tree(self, branching_factor):
+        """
+            Build a full search tree with a given branching factor and depth.
+
+            In the original OLOP paper, the tree is built in advance so that leaves at depth L can be seen
+            as arms of a structured bandit problem.
+        :param branching_factor: The number of actions in each state
+        """
+        for _ in range(self.config["horizon"]):
+            next_leaves = []
+            for leaf in self.leaves:
+                super(OLOPNode, leaf).expand(branching_factor)
+                next_leaves += leaf.children.values()
+            self.leaves = next_leaves
 
     @staticmethod
     def horizon(episodes, gamma):
         return int(np.ceil(np.log(episodes) / (2 * np.log(1 / gamma))))
 
     def allocate_budget(self):
-        for episodes in range(1, 1000):
+        """
+            Allocate the computational budget into M episodes of fixed horizon L.
+        """
+        for episodes in range(1, self.config["budget"]):
             if episodes * OLOP.horizon(episodes, self.config["gamma"]) > self.config["budget"]:
                 self.config["episodes"] = episodes - 1
                 self.config["horizon"] = OLOP.horizon(self.config["episodes"], self.config["gamma"])
@@ -53,21 +82,26 @@ class OLOP(AbstractPlanner):
         """
         # Compute B-values
         list(Node.breadth_first_search(self.root, operator=self.compute_u_values, condition=None))
-        sequences = list(map(OLOP.compute_b_values, self.leaves))
+        sequences_upper_bounds = list(map(OLOP.sharpen_b_values, self.leaves))
 
-        # Pick best action sequence
-        best_sequence = list(self.leaves[np.argmax(sequences)].path())
+        # Pick best sequence of actions
+        best_sequence = list(self.leaves[np.argmax(sequences_upper_bounds)].path())
 
-        # Execute sequence and collect rewards
+        if self.config["lazy_tree_construction"]:
+            # If the sequence length is shorter than the horizon, all continuations have the same upper-bounds.
+            # Pick one continuation arbitrarily. Here, pad with the sequence [1, ...., 1].
+            best_sequence = best_sequence[:self.config["horizon"]] + [1]*(self.config["horizon"] - len(best_sequence))
+
+        # Execute sequence, expand tree if needed, collect rewards and update upper confidence bounds.
         node = self.root
         terminal = False
         for action in best_sequence:
             observation, reward, done, _ = state.step(action)
             terminal = terminal or done
+            if action not in node.children:
+                node.expand(state, self.leaves, update_children=False)
             node = node.children[action]
             node.update(reward, done)
-        if len(best_sequence) < self.config["horizon"]:
-            node.expand(state, self.leaves)
 
     def compute_u_values(self, node, path):
         """
@@ -77,7 +111,7 @@ class OLOP(AbstractPlanner):
             It is computed by summing upper bounds of intermediate rewards along the sequence, and an upper bound
             of the remaining rewards over possible continuations of the sequence.
         :param node: a node in the look-ahead tree
-        :param path: the path from the root to the node (unused)
+        :param path: the path from the root to the node
         :return: the path from the root to the node, and the node value.
         """
         # Upper bound of the reward-to-go after this node
@@ -89,20 +123,24 @@ class OLOP(AbstractPlanner):
         return path, node.value
 
     @staticmethod
-    def compute_b_values(node):
+    def sharpen_b_values(node):
         """
             Sharpen the upper-bound value of the action sequences at the tree leaves.
 
             By computing the min over intermediate upper-bounds along the sequence, that must all be satisfied.
+            If the KL-UCB are used, the leaf always has the lowest value UCB and no further sharpening can be achieved.
         :param node: a node in the look-ahead tree
-        :return:the sharpened upper-bound
+        :return:an upper-bound of the sequence value
         """
-        node_t = node
-        min_ucb = node.value
-        while node_t.parent:
-            min_ucb = min(min_ucb, node_t.value)
-            node_t = node_t.parent
-        return min_ucb
+        if node.planner.config["upper_bound"] == "kullback-leibler":
+            return node.value
+        else:
+            node_t = node
+            min_value = node.value
+            while node_t.parent:
+                min_value = min(min_value, node_t.value)
+                node_t = node_t.parent
+            return min_value
 
     def plan(self, state, observation):
         for i in range(self.config['episodes']):
@@ -125,6 +163,9 @@ class OLOPNode(Node):
         self.mu_ucb = np.infty
         """ Upper bound of the node mean reward. """
 
+        if self.planner.config["upper_bound"] == "kullback-leibler":
+            self.mu_ucb = 1
+
         self.done = False
         """ Is this node a terminal node, for all random realizations (!)"""
 
@@ -134,19 +175,19 @@ class OLOPNode(Node):
         counts = Node.all_argmax([self.children[a].count for a in actions])
         return actions[max(counts, key=(lambda i: self.children[actions[i]].get_value()))]
 
-    def update(self, reward, done, upper_bound="kullback-leibler"):
+    def update(self, reward, done):
         if not 0 <= reward <= 1:
             raise ValueError("This planner assumes that all rewards are normalized in [0, 1]")
         self.cumulative_reward += reward
         self.count += 1
-        if upper_bound == "hoeffding":
+        if self.planner.config["upper_bound"] == "hoeffding":
             self.mu_ucb = hoeffding_upper_bound(self.cumulative_reward, self.count, self.planner.config["episodes"])
-        if upper_bound == "kullback-leibler":
+        elif self.planner.config["upper_bound"] == "kullback-leibler":
             self.mu_ucb = kl_upper_bound(self.cumulative_reward, self.count, self.planner.config["episodes"])
         if done and OLOPNode.STOP_ON_ANY_TERMINAL_STATE:
             self.done = True
 
-    def expand(self, state, leaves):
+    def expand(self, state, leaves, update_children=False):
         if state is None:
             raise Exception("The state should be set before expanding a node")
         try:
@@ -156,8 +197,9 @@ class OLOPNode(Node):
         for action in actions:
             self.children[action] = type(self)(self,
                                                self.planner)
-            _, reward, done, _ = safe_deepcopy_env(state).step(action)
-            self.children[action].update(reward, done)
+            if update_children:
+                _, reward, done, _ = safe_deepcopy_env(state).step(action)
+                self.children[action].update(reward, done)
 
         leaves.remove(self)
         leaves.extend(self.children.values())
