Fix KL-UCB test

rl_agents/agents/utils.py
@@ -149,7 +149,7 @@ def hoeffding_upper_bound(_sum, count, time):
     return _sum / count + np.sqrt(2 * np.log(time) / count)
 
 
-def kl_upper_bound(_sum, count, time, eps=1e-2, c=4):
+def kl_upper_bound(_sum, count, time, c=2, eps=1e-2):
     """
         Upper Confidence Bound of the empirical mean built on the Kullback-Leibler divergence.
 
@@ -158,6 +158,7 @@ def kl_upper_bound(_sum, count, time, eps=1e-2, c=4):
     :param _sum: Sum of sample values
     :param count: Number of samples
     :param time: Allows to set the bound confidence level
+    :param c: Coefficient before the log(t) in the maximum divergence
     :param eps: Absolute accuracy of the Netwon Iteration
     """
     mu = _sum/count

tests/agents/test_utils.py
@@ -25,14 +25,14 @@ def test_d_bernoulli_kullback_leibler_dq():
 
 
 def test_kl_upper_bound():
-    assert kl_upper_bound(0.5 * 1, 1, 10, eps=1e-3) == pytest.approx(0.997, abs=1e-3)
-    assert kl_upper_bound(0.5 * 10, 10, 20, eps=1e-3) == pytest.approx(0.835, abs=1e-3)
-    assert kl_upper_bound(0.5 * 20, 20, 40, eps=1e-3) == pytest.approx(0.777, abs=1e-3)
+    assert kl_upper_bound(0.5 * 1, 1, 10, c=1, eps=1e-3) == pytest.approx(0.997, abs=1e-3)
+    assert kl_upper_bound(0.5 * 10, 10, 20, c=1, eps=1e-3) == pytest.approx(0.835, abs=1e-3)
+    assert kl_upper_bound(0.5 * 20, 20, 40, c=1, eps=1e-3) == pytest.approx(0.777, abs=1e-3)
 
     rands = np.random.randint(1, 500, 2)
     rands.sort()
     mu, count, time = np.random.random(), rands[0], rands[1]
-    ucb = kl_upper_bound(mu*count, count, time, eps=1e-3)
+    ucb = kl_upper_bound(mu*count, count, time, c=1, eps=1e-3)
     assert not np.isnan(ucb)
-    d_max = np.log(time) / count
+    d_max = 1 * np.log(time) / count
     assert bernoulli_kullback_leibler(mu, ucb) == pytest.approx(d_max, abs=1e-2)