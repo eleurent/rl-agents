8b49f2735fd932fb53283436ef89934431da8448
Edouard Leurent <eleurent@gmail.com>
2019-03-21 15:33:25 +0100

    By default, resume run instead of clearing it

--- rl_agents/trainer/monitor.py
+++ rl_agents/trainer/monitor.py
@@ -27,7 +27,7 @@ class MonitorV2(Monitor):
     """
     RUN_PREFIX = 'run'
 
-    def __init__(self, env, directory, add_subdirectory=True, video_callable=None, force=True, resume=False,
+    def __init__(self, env, directory, add_subdirectory=True, video_callable=None, force=False, resume=True,
                  write_upon_reset=False, uid=None, mode=None):
         if add_subdirectory:
             directory = self.run_directory(directory)
