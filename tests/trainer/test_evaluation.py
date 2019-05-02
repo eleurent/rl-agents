dac0be3a64cebb713dacef276c091bc998f2aebd
Edouard Leurent <eleurent@gmail.com>
2019-05-02 16:57:59 +0200

    Fix evaluation test: artifacts are now in the run directory

--- tests/trainer/test_evaluation.py
+++ tests/trainer/test_evaluation.py
@@ -1,47 +1,21 @@
+from pathlib import Path
 import gym
 
-from rl_agents.agents.common.abstract import AbstractAgent
+from rl_agents.agents.simple.random import RandomUniformAgent
 from rl_agents.trainer.evaluation import Evaluation
 
 
 def test_evaluation(tmpdir):
     env = gym.make('CartPole-v0')
-    agent = RandomAgent(env)
+    agent = RandomUniformAgent(env)
     evaluation = Evaluation(env,
                             agent,
-                            directory=tmpdir.strpath,
+                            directory=tmpdir,
                             num_episodes=3,
                             display_env=False,
                             display_agent=False,
                             display_rewards=False)
-    evaluation.monitor._monitor = True  # TODO: dirty fix until merge of https://github.com/openai/gym/pull/1362
     evaluation.train()
-    artifacts = tmpdir.listdir()
-    assert any(['manifest' in file.basename for file in artifacts])
-    assert any(['metadata' in file.basename for file in artifacts])
-    assert any(['stats' in file.basename for file in artifacts])
-
-
-class RandomAgent(AbstractAgent):
-
-    def __init__(self, env):
-        super(RandomAgent, self).__init__()
-        self.env = env
-
-    def record(self, state, action, reward, next_state, done, info):
-        pass
-
-    def act(self, state):
-        return self.env.action_space.sample()
-
-    def reset(self):
-        pass
-
-    def seed(self, seed=None):
-        pass
-
-    def save(self, filename):
-        pass
-
-    def load(self, filename):
-        pass
+    assert any(['manifest' in file.name for file in evaluation.run_directory.iterdir()])
+    assert any(['metadata' in file.name for file in evaluation.run_directory.iterdir()])
+    assert any(['stats' in file.name for file in evaluation.run_directory.iterdir()])
