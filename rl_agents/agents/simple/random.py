e84cb7fe5f507145544501adea0edc0bb75170ab
Edouard Leurent <eleurent@gmail.com>
2019-05-02 17:35:26 +0200

    Fix seeding of action spaces, since gym.spaces do not use gym.seeding...

--- rl_agents/agents/simple/random.py
+++ rl_agents/agents/simple/random.py
@@ -22,6 +22,10 @@ class RandomUniformAgent(AbstractAgent):
         :param seed: the seed to be used
         :return: the used seed
         """
+        # Note: gym spaces use np.RandomState instead of gym.seeding.
+        # Hence, the seed must be a uint32 and instead of a long int.
+        if seed:
+            div, seed = divmod(seed, 2**32)
         return self.env.action_space.seed(seed)
 
     def save(self, filename):
