94a1193deb6b335f8ef010bdea236d772d43e3b9
Edouard Leurent <eleurent@gmail.com>
2019-09-12 13:35:07 +0200

    Remove dead code

--- rl_agents/agents/deep_q_network/pytorch.py
+++ rl_agents/agents/deep_q_network/pytorch.py
@@ -63,12 +63,6 @@ class DQNAgent(AbstractDQNAgent):
                 target_state_action_value = batch.reward + self.config["gamma"] * next_state_values
 
         # Compute loss
-        # print("saving state_action_values.pt...")
-        # torch.save(state_action_values, 'state_action_values.pt')
-        # print("saving target_state_action_value.pt...")
-        # torch.save(target_state_action_value, 'target_state_action_value.pt')
-        # print("model min", torch.min(state_action_values), "max", torch.max(state_action_values), "nan", torch.isnan(state_action_values).any())
-        # print("target min", torch.min(target_state_action_value), "max", torch.max(target_state_action_value), "nan", torch.isnan(target_state_action_value).any())
         loss = self.loss_function(state_action_values, target_state_action_value)
         return loss, target_state_action_value, batch
 
