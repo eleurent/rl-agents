738b23901609a1a036138d4ac55d6009913e4dd5
Edouard Leurent <eleurent@gmail.com>
2019-10-23 18:45:46 +0200

    Refactor state aware planner

--- rl_agents/agents/tree_search/state_aware.py
+++ rl_agents/agents/tree_search/state_aware.py
@@ -1,130 +1,97 @@
 import numpy as np
 import logging
 from rl_agents.agents.common.factory import safe_deepcopy_env
-from rl_agents.agents.tree_search.abstract import Node, AbstractTreeSearchAgent, AbstractPlanner
+from rl_agents.agents.tree_search.abstract import Node, AbstractPlanner
+from rl_agents.agents.tree_search.deterministic import DeterministicPlannerAgent, OptimisticDeterministicPlanner, \
+    DeterministicNode
 
 logger = logging.getLogger(__name__)
 
 
-class StateAwarePlannerAgent(AbstractTreeSearchAgent):
+class StateAwarePlannerAgent(DeterministicPlannerAgent):
     """
-        An agent that performs optimistic planning in deterministic MDPs.
+        An agent that performs optimistic planning in deterministic MDPs with states.
     """
     def make_planner(self):
         return StateAwarePlanner(self.env, self.config)
 
 
-class StateAwarePlanner(AbstractPlanner):
+class StateAwarePlanner(OptimisticDeterministicPlanner):
     """
        An implementation of State Aware Planning.
     """
     def __init__(self, env, config=None):
-        super().__init__(config)
-        self.env = env
-        self.leaves = None
-        self.state_nodes = {}
+        super().__init__(env, config)
+        self.state_nodes = {}  # Mapping of states to tree nodes that lead to this state
+        self.state_values = {}  # Mapping of states to upper confidence bounds of the state-value
 
     def make_root(self):
         root = StateAwareNode(None, planner=self)
         self.leaves = [root]
         return root
 
-    def run(self):
+    def update_value(self, observation, value):
         """
-            Run an OptimisticDeterministicPlanner episode
-        """
-        leaf_to_expand = max(self.leaves, key=lambda n: n.get_value_upper_bound())
-        if not leaf_to_expand.done:
-            leaf_to_expand.expand(self.leaves)
+            Update the upper-confidence-bound for the value of a state with a possibly tighter candidate
 
-        leaf_to_expand.backup_to_root()
+        :param observation: an observed state
+        :param value: a candidate upper-confidence bound
+        :return: whether or not the value was updated, and needs to be backed-up
+        """
+        if str(observation) not in self.state_values or value < self.state_values[str(observation)]:
+            self.state_values[str(observation)] = value
+            return True
+        else:
+            return False
 
     def plan(self, state, observation):
+        # Initialize root
         self.root.state = state
         self.root.observation = observation
         self.state_nodes[str(observation)] = [self.root]
+        self.state_values[str(observation)] = 1 / (1 - self.config["gamma"])
+
+        # Plan
         for _ in np.arange(self.config["budget"] // state.action_space.n):
             self.run()
-        print(self.config["budget"] // state.action_space.n, "expansions")
-        print(len(self.state_nodes), "states explored")
+        logger.debug("{} expansions".format(self.config["budget"] // state.action_space.n))
+        logger.debug("{} states explored".format(len(self.state_nodes)))
 
         return self.get_plan()
 
 
-class StateAwareNode(Node):
-    def __init__(self, parent, planner, state=None, observation=None, depth=0):
-        super().__init__(parent, planner)
-        self.state = state
+class StateAwareNode(DeterministicNode):
+    def __init__(self, parent, planner, state=None, depth=0):
+        super().__init__(parent, planner, state, depth)
+        self.observation = None  # Store observations
+
+    def update(self, reward, done, observation=None):
+        super().update(reward, done)
         self.observation = observation
-        self.depth = depth
-        self.reward = 0
-        self.value = 0  # Sum of rewards along sequence
-        self.future_value_upper_bound = 0  # Upper-bound on optimal future rewards
-        self.count = 1
-        self.done = False
 
-    def selection_rule(self):
-        if not self.children:
-            return None
-        actions = list(self.children.keys())
-        index = self.random_argmax([self.children[a].get_value() for a in actions])
-        return actions[index]
+        # Add to list of nodes with this observation
+        if str(observation) not in self.planner.state_nodes:
+            self.planner.state_nodes[str(observation)] = []
+        self.planner.state_nodes[str(observation)].append(self)
 
-    def expand(self, leaves):
-        leaves.remove(self)
-        if self.state is None:
-            raise Exception("The state should be set before expanding a node")
-        try:
-            actions = self.state.get_available_actions()
-        except AttributeError:
-            actions = range(self.state.action_space.n)
-        for action in actions:
-            self.children[action] = type(self)(self,
-                                               self.planner,
-                                               state=safe_deepcopy_env(self.state),
-                                               depth=self.depth + 1)
-            observation, reward, done, _ = self.children[action].state.step(action)
-            leaves.append(self.children[action])
-            self.children[action].update(reward, done, observation, leaves)
+        # Update the ucb for the value of this state
+        future_value_ucb = 1/(1 - self.planner.config["gamma"]) if not self.done else 0  # Default value
+        self.planner.update_value(observation, future_value_ucb)  # Aggregate with other nodes
 
-    def update(self, reward, done, observation, leaves):
-        if not np.all(0 <= reward) or not np.all(reward <= 1):
-            raise ValueError("This planner assumes that all rewards are normalized in [0, 1]")
-        gamma = self.planner.config["gamma"]
-        self.reward = reward
-        self.observation = observation
-        self.value = self.parent.value + (gamma ** (self.depth - 1)) * reward
-        self.done = done
-        if self.done:
-            self.future_value_upper_bound = 0
-        else:
-            if str(observation) not in self.planner.state_nodes:
-                self.planner.state_nodes[str(observation)] = []
-            self.planner.state_nodes[str(observation)].append(self)
-            same_state_nodes = self.planner.state_nodes[str(observation)]
-
-            # Set min state UCB for all occurences
-            future_value_upper_bound = min([1/(1 - gamma)] + [node.future_value_upper_bound for node in same_state_nodes])
-            for node in same_state_nodes:
-                node.future_value_upper_bound = future_value_upper_bound
-
-            # Pick the one with highest sequence-value, and kill the others
-            best = max([n for n in same_state_nodes], key=lambda n: n.get_value_upper_bound())
-            for node in same_state_nodes:
-                if node is not best and not node.children and node in leaves:
-                    leaves.remove(node)
+        # Among sequences that lead to this state, remove all suboptimal leaves
+        best = max([n for n in self.planner.state_nodes[str(observation)]], key=lambda n: n.get_value_upper_bound())
+        for node in self.planner.state_nodes[str(observation)]:
+            if node is not best and not node.children and node in self.planner.leaves:
+                self.planner.leaves.remove(node)
 
     def backup_to_root(self):
         if self.parent:
-            gamma = self.planner.config["gamma"]
-            best_child = max(self.parent.children.values(), key=lambda child: child.get_value_upper_bound())
-
-            # Here, we could overwrite a state value U_a^s that was previously transfered from elsewhere
-            # by another value backed up from the children. This is probably fine.
-            self.future_value_upper_bound = best_child.reward + gamma*best_child.future_value_upper_bound
-
-            self.parent.backup_to_root()
             self.count += 1
+            best_child = max(self.parent.children.values(), key=lambda child: child.get_value_upper_bound())
+            self.planner.update_value(self.observation, best_child.reward + self.planner.config["gamma"] *
+                                      self.planner.state_values[str(best_child.observation)])
+            self.parent.backup_to_root()
 
     def get_value_upper_bound(self):
-        return self.value + (self.planner.config["gamma"] ** self.depth) * self.future_value_upper_bound
+        return self.value + \
+               (self.planner.config["gamma"] ** self.depth) * self.planner.state_values[str(self.observation)]
