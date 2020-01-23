045d7167c3648e291a353179eb59c9028d69c492
Edouard Leurent <eleurent@gmail.com>
2020-01-23 17:56:13 +0100

    Update line_env and config

--- utils/envs/gridenv.py
+++ utils/envs/gridenv.py
@@ -57,6 +57,7 @@ class LineEnv(Env):
         self.x = 0
         self.action_space = spaces.Discrete(2)
         self.np_random = None
+        self.done = False
         self.seed()
 
     def step(self, action):
@@ -68,16 +69,18 @@ class LineEnv(Env):
         # Noise
         delta += 2*self.np_random.randint(2) - 1
         self.x += delta // 2
-        return self.x, self.reward(), self.terminal(), {}
+        self.done = self.done or self.terminal()
+        return self.x, self.reward(), self.done, {}
 
     def reward(self):
-        return 1 / (1 + self.x ** 2)
+        return 1 if not self.done else 0
 
     def terminal(self):
         return abs(self.x) >= 2
 
     def reset(self):
         self.x = 0
+        self.done = False
         return self.x
 
     def render(self, mode='human'):
@@ -95,5 +98,5 @@ register(
 register(
     id='line_env-v0',
     entry_point='utils.envs:LineEnv',
-    max_episode_steps=8
+    max_episode_steps=10
 )
