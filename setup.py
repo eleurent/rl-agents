ebb8ac8eee4e9c023b887bdcc37b3bba9e037319
Edouard Leurent <eleurent@gmail.com>
2020-10-21 12:07:54 +0200

    Add numba dependency

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
 
     keywords='reinforcement learning agents',
     packages=find_packages(exclude=['docs', 'scripts', 'tests*']),
-    install_requires=['gym', 'numpy', 'pandas', 'pygame', 'matplotlib', 'seaborn', 'six', 'docopt', 
+    install_requires=['gym', 'numpy', 'pandas', 'numba', 'pygame', 'matplotlib', 'seaborn', 'six', 'docopt',
                       'torch>=1.2.0', 'tensorboardX'],
     tests_require=['pytest'],
     extras_require={
